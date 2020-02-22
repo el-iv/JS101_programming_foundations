f9f9de785beb5a9ba930f51c3d2dd56ce170b1d0
elodie <elodie.iv@gmail.com>
2020-02-21 23:13:51 -1000

    quick draft code

---
+++ lesson_2/car_loan_calculator.js
@@ -0,0 +1,20 @@
+let readline = require('readline-sync');
+
+console.log('Enter the loan amount in $: ');
+let loanAmount = readline.question();
+loanAmount = Number(loanAmount);
+
+console.log('Enter the annual interest rate, for example 0.10: ');
+let annualInterestRate = readline.question();
+annualInterestRate = Number(annualInterestRate);
+
+console.log('Enter the loan duration: ');
+let loanDurationMonths = readline.question();
+loanDurationMonths = Number(loanDurationMonths);
+
+let monthlyInterestRate = annualInterestRate / 12;
+let monthlyPayment = loanAmount *
+                  (monthlyInterestRate /
+                  (1 - Math.pow(1 + monthlyInterestRate, -loanDurationMonths)));
+
+console.log(`The monthly interest payment is: ${monthlyPayment.toFixed(2)}`);
